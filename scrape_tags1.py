86d4ca4659409875bf4c8911afcb9b591ab43ca3
KFCAsianWonton <40906282+KFCAsianWonton@users.noreply.github.com>
2021-11-28 14:25:10 -0600

    Extracting Tags

--- scrape_tags1.py
+++ scrape_tags1.py
@@ -8,3 +8,25 @@ from scrapetest import getTitle
 html = requests.get('http://www.mikiyakobayashi.com/projects')
 bs = BeautifulSoup(html.read(), 'html.parser')
 
+pictureList = bs.find_all('img src')
+index = 1
+for picture in pictureList:
+    print(f'Picture #{index} is {picture}')
+    index += 1
+
+""" There's an easy way to scale these for loops, but for redundancy sake I am keeping them separate. """
+
+""" I'm trying to figure out how to shorten the html link so that I can extract the ttle of the photo. """
+""" I guess that's where you have to use text processing """
+linkList = bs.find_all('a href')
+index = 1
+for link in linkList:
+    print(f'Link #{index} is {link}')
+    index += 1
+
+""" Parse the link. """
+def linkParser(link):
+    str.replace(link, '/', ' ')
+
+    return link
+
