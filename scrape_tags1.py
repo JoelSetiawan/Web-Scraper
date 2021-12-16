b8902cfb5efd4b728db0186ff72bdeb76894ca3d
KFCAsianWonton <40906282+KFCAsianWonton@users.noreply.github.com>
2021-12-16 16:47:18 -0600

    Formatting in OOP Style, switching to requests library

--- scrape_tags1.py
+++ scrape_tags1.py
@@ -1,34 +1,81 @@
 # Extract Tags
 from bs4 import BeautifulSoup
 
+import sys
+
 import requests
+
 import re
 
-# Main file
-import scrapetest
+""" The plan is to create a web scraper that relies on input from my program, then we expand it to taking in user input, then expand the features
+, allowing users to, when searching for it have a trie data structure for autocorrection, and then we expand it to saving the record 
+of the data we have scraped onto a file/database, and/or organizes this. """
 
-html = requests.get('http://www.mikiyakobayashi.com/projects')
-bs = BeautifulSoup(html.read(), 'html.parser')
+""" Let's break down my steps 
 
-pictureList = bs.find_all('img src')
-index = 1
-for picture in pictureList:
-    print(f'Picture #{index} is {picture}')
-    index += 1
+    1. Learn how BeautifulSoup works
+    2. Let People Search for Things they want with the web scraper
+        People should be able to write the link of the website they want to scrape
+            In the future, create a feature that allows them to choose what html attribute they want to scrape.
+    3. Let Myself Search a website for keywords or sentences that I want to scrape
+        Potential:
+        Job Posting Keywords
+        Literature
+        Metadata that tells me something has updated
+        Notifications on Website Updates
 
-""" There's an easy way to scale these for loops, but for redundancy sake I am keeping them separate. """
+    4. Data Structures and Algorithms I will have to use:
+        Binary Search - To Search for Important Stuff
+        Knowing how to traverse the BeautifulSoup tree
+        LinkedList
+        Regular Lists
+        Text Processing
+        Creating a Database
+            Requires A Record Data Structure with Fields for Queries
 
-""" I'm trying to figure out how to shorten the html link so that I can extract the ttle of the photo. """
-""" I guess that's where you have to use text processing """
-linkList = bs.find_all('a href')
-index = 1
-for link in linkList:
-    print(f'Link #{index} is {link}')
-    index += 1
+    Important things to remember
+        Code Readibility and Maintainability
+        Work on small chunks of the project
+        Don't forget the function is the most important aspect
+        Automatic means it runs on its own, learn how automatic works.
+        Persevere Through, It will get Boring
+        Clean your code and writing.
 
-""" Parse the link. """
-def linkParser(link):
-    str.replace(link, '/', ' ')
+If I try to import another file as a header, python decides to run the whole other file before this file. 
+We can scale this requests by allowing the link to come from user input or a file. """
 
-    return link
+
+def main(argv):
+    html = requests.get('http://www.mikiyakobayashi.com')
+    
+    SoupObject = BeautifulSoup(html.text, 'html.parser');
+    prettifiedSoup = SoupObject.prettify()
+
+    print(f'This is the prettified version of our beautiful soup: ' + prettifiedSoup)
+
+    images = SoupObject.find_all('img')
+    print(images)
+
+if __name__ == "__main__":
+    main(sys.argv) 
+
+# index = 0
+# for picture in pictureList:
+#     print(f'Picture #{index} is {picture}')
+#     index += 1
+
+# """ There's an easy way to scale these for loops, but for redundancy sake I am keeping them separate. """
+
+# """ I'm trying to figure out how to shorten the html link so that I can extract the ttle of the photo. """
+# """ I guess that's where you have to use text processing """
+# linkList = bs.find_all('a href')
+# index = 1
+# for link in linkList:
+#     print(f'Link #{index} is {link}')
+#     index += 1
+
+# """ Parse the link. """
+# def linkParser(link):
+#     str.replace(link, '/', ' ')
+#     return link
 
--- scrapetest.py
+++ scrapetest.py
@@ -3,43 +3,46 @@ from urllib.request import urlcleanup, urlopen
 from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
+# Requests is urllib3, the better version of urllib.
 import requests
+
+# Re stands for Regular Expressions
 import re
 
+import sys
 
 def getTitle(url):
     try:
-        html = urlopen(url)
+        html = requests.get(url)
     except HTTPError as e:
         return None
-    try:
-        bs = BeautifulSoup(html.read(), 'html.parser')
+    finally:
+        bs = BeautifulSoup(html.text, 'html.parser')
         title = bs.body.h1
-    except AttributeError as e:
-        return None
     return title
 
-link1 = 'http://leeandlie.blogspot.com/2016/04/again-english-lyrics.html'
-title = getTitle(link1)
-if title == None:
-    print('Title could not be found')
-else:
-    print(title)
+def main(argv):
+    link1 = 'http://leeandlie.blogspot.com/2016/04/again-english-lyrics.html'
+    title = getTitle(link1)
 
-try: 
-    html = urlopen(link1)
-except HTTPError as e:
-    print('You got an HTTP error!')
-except AttributeError as e:
-    print('You weren\'t able to get the attribute that you wanted')
-finally: 
-    bs = BeautifulSoup(html.read(), 'html.parser')
-    print('It worked!')
-    scrape_lyrics = bs
-    pretty_lyrics = scrape_lyrics.decode(pretty_print=True, eventual_encoding='utf-8', formatter='minimal')
-    print(pretty_lyrics)
-    print('\n\n\n\n')
+    if title == None:
+        print('Title could not be found')
+    else:
+        print(title)
 
+    try: 
+        html = urlopen(link1)
+    except HTTPError as e:
+        print('You got an HTTP error!')
+    except AttributeError as e:
+        print('You weren\'t able to get the attribute that you wanted')
+    finally: 
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        print('It worked!')
+        scrape_lyrics = bs
+        pretty_lyrics = scrape_lyrics.decode(pretty_print=True, eventual_encoding='utf-8', formatter='minimal')
+        print(pretty_lyrics)
+        print('\n\n\n\n')
 
-
-
+if __name__ == "__main__":
+    main(sys.argv)
